7b1fae34c0e45f8c267e2faa5e6e5f1718371766
Jiří Papoušek <jipapous@redhat.com>
2024-07-18 10:29:22 +0200

    Extend build script with v2 configurations

--- build.sh
+++ build.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
 mkdir -p ./build
+mkdir -p ./build/v1
+mkdir -p ./build/v2
 
-# Build the conditions
+# Build the conditions for v1 endpoint
 VERSION=$(git describe --tags --abbrev=0)
 jq --arg VERSION $VERSION \
-    -s '{ version: $VERSION, rules: map(.) }' ./conditions/*.json > ./build/rules.json
+    -s '{ version: $VERSION, rules: map(.) }' ./conditions/*.json > ./build/v1/rules.json
+
+# Build remote configurations
+for config_template in ./remote_configurations/*; do
+    # build JSON array with contents of all required conditions
+    contents='['
+    while read -r condition; do 
+	contents+=$(cat conditions/${condition}.json)
+	contents+=','
+    done < <(jq -r '.conditional_gathering_rules[]' $config_template)
+    # remove last comma from the array
+    contents=${contents%?}
+    contents+=']'
+    # build entire config with contents and the version
+    jq --argjson contents "${contents}" --arg VERSION $VERSION '.version=$VERSION | .conditional_gathering_rules=$contents' $config_template > ./build/v2/$(basename $config_template)
+done
