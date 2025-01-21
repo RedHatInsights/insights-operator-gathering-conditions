b400989245cd168f5666426fdf377b733b3a7856
ikerreyes <24312587+ikerreyes@users.noreply.github.com>
2025-01-21 10:31:51 +0100

    Golang tool to validate regexes passed via STDIN (#27)
    * Golang tool to validate regexes passed via STDIN
    
    Co-authored-by: Jan Holeček <71874510+jholecek-rh@users.noreply.github.com>

---
+++ tests/golang_regex_validator/test_regex_compiler.py
@@ -0,0 +1,38 @@
+import subprocess
+from os import path
+from pathlib import Path
+
+import pytest
+
+_FOLDER = Path(path.abspath(__file__)).parent
+_GO_TOOL_FILE = _FOLDER.parent.parent / "golang_regex_validator/regexCompiler.go"
+
+INVALID_REGEX = "[\\]"
+
+
+def test_can_locate_go_tool_file():
+    """Ensure we point to the right file"""
+    assert _GO_TOOL_FILE.exists()
+
+
+def invoke(input: str) -> subprocess.CompletedProcess:
+    return subprocess.run(
+        ["go", "run", _GO_TOOL_FILE], input=input, capture_output=True, encoding="utf-8"
+    )
+
+
+@pytest.mark.parametrize(
+    "regex,valid",
+    [
+        ("ab", True),
+        (INVALID_REGEX, False),
+    ],
+)
+def test_exit_status(regex, valid):
+    cp = invoke(regex)
+    assert cp.returncode == 0 if valid is True else cp.returncode != 0
+
+
+def test_errors_are_on_stderr():
+    cp = invoke(INVALID_REGEX)
+    assert cp.stderr != None
