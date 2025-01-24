bf2fc486302e64e666102faff5b1331caf21b464
Iker Reyes <ireyes@redhat.com>
2025-01-24 11:11:18 +0100

    Add file to improve the quality of the invalid regex messages

--- tests/source_data_validation/test_regexes.py
+++ tests/source_data_validation/test_regexes.py
@@ -2,6 +2,7 @@ import json
 
 import pytest
 
+from tests import PROJECT_ROOT
 from tests.source_data_validation import CONTAINER_LOG_REQUESTS
 
 
@@ -10,7 +11,7 @@ def get_pod_name_regexes():
     regexes = set()
     for file in CONTAINER_LOG_REQUESTS.glob("**/*.json"):
         data = json.loads(file.read_text())
-        regexes.add(data["pod_name_regex"])
+        regexes.add((file.relative_to(PROJECT_ROOT), data["pod_name_regex"]))
     return sorted(regexes)  # make a consistent order if we use xdist
 
 
@@ -19,15 +20,20 @@ def get_message_filters():
     filters = set()
     for file in CONTAINER_LOG_REQUESTS.glob("**/*.json"):
         data = json.loads(file.read_text())
-        filters.update(data["messages"])
+        for message in data["messages"]:
+            filters.add((file.relative_to(PROJECT_ROOT), message))
     return sorted(filters)  # make a consistent order if we use xdist
 
 
-@pytest.mark.parametrize("name", get_pod_name_regexes())
-def test_pod_names_are_valid(golang_regex_validator, name):
-    assert golang_regex_validator.run(name).returncode == 0, f"Invalid regex {repr(name)}"
+@pytest.mark.parametrize("file_and_name", get_pod_name_regexes())
+def test_pod_names_are_valid(golang_regex_validator, file_and_name):
+    file, name = file_and_name
+    assert golang_regex_validator.run(name).returncode == 0, f"Invalid regex {repr(name)} in {file}"
 
 
-@pytest.mark.parametrize("filter", get_message_filters())
-def test_message_filters(golang_regex_validator, filter):
-    assert golang_regex_validator.run(filter).returncode == 0, f"Invalid regex {repr(filter)}"
+@pytest.mark.parametrize("file_and_filter", get_message_filters())
+def test_message_filters(golang_regex_validator, file_and_filter):
+    file, filter = file_and_filter
+    assert golang_regex_validator.run(filter).returncode == 0, (
+        f"Invalid regex {repr(filter)} in {file}"
+    )
