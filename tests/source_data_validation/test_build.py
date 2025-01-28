7251acad8ae2ae227c669e8ef48ef1c38b88d210
Jakub007d <dr.kubko@gmail.com>
2025-01-28 12:39:53 +0100

    Changing tmpdir fixture to tmp_path

--- tests/source_data_validation/test_build.py
+++ tests/source_data_validation/test_build.py
@@ -1,9 +1,9 @@
 import pathlib
 
 
-def test_build_tool(build_tool_validator, tmpdir):
+def test_build_tool(build_tool_validator, tmp_path):
     # a test probably should not override the default build directory
-    outputdir = pathlib.Path(tmpdir)
+    outputdir = tmp_path
     cp = build_tool_validator.run("--outputdir", outputdir)
     assert cp.returncode == 0
     assert (outputdir / "v1" / "rules.json").is_file()
