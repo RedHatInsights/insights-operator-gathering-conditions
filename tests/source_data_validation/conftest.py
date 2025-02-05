4d27e9bb01b491d48f97b27b6678e8f97bf6f193
Jiří Papoušek <jipapous@redhat.com>
2025-02-05 10:12:57 +0100

    Remove unused fixture

--- tests/source_data_validation/conftest.py
+++ tests/source_data_validation/conftest.py
@@ -10,11 +10,6 @@ from tests.source_data_validation import (
 )
 
 
-@pytest.fixture(name="container_log_requests")
-def container_log_requests_fixture():
-    return container_log_requests()
-
-
 @pytest.fixture(name="remote_configurations")
 def remote_configurations_fixture():
     return remote_configurations()
