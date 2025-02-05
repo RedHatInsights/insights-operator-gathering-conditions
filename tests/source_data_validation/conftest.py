c841927627076d12e258c8f0faab956fcadf6cdd
Jiří Papoušek <jipapous@redhat.com>
2025-02-05 12:34:33 +0100

    Update tests/source_data_validation/conftest.py
    Co-authored-by: Jan Holeček <71874510+jholecek-rh@users.noreply.github.com>

--- tests/source_data_validation/conftest.py
+++ tests/source_data_validation/conftest.py
@@ -6,7 +6,7 @@ from referencing import Registry, Resource
 from tests.source_data_validation import SCHEMAS
 
 
-@pytest.fixture
+@pytest.fixture(scope="session")
 def schema_registry():
     def retrieve_schema(schema_ref):
         return Resource.from_contents(json.loads((SCHEMAS / schema_ref).read_text()))
